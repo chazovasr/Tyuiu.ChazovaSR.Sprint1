62af0cfee9c5cb81332a32071e942c83c74e5770
Tyuiu.ChazovaSR@outlook.com <chazovas0@gmail.com>
2023-09-18 11:27:27 +0500

    Add project files.

---
+++ Tyuiu.ChazovaSR.Sprint1.Task0.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChazovaSR.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChazovaSR.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b9bf65a-687f-40b5-a244-866ae4a5ff30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ChazovaSR.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChazovaSR.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.ChazovaSR.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Чазова С.Р | ИСТНб-23-1";
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* Спринт #1                                         *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                  *");
+            Console.WriteLine("* Задание #0                                        *");
+            Console.WriteLine("* Вариант #0                                        *");
+            Console.WriteLine("* Выполнила: Чазова Софья Романовна | ИСТНб-23-1    *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                           ");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("*Написать программу на С#, вычисляет выражение       ");
+            Console.WriteLine("* 10 / (2 + 3) и печатает результат на экране.      *");
+            Console.WriteLine("*                                                   *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                  *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* 10 / (2 + 3)                                       ");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                         ");
+            Console.WriteLine("*****************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
